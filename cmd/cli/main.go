fe28dcf4c1eddc8150052b995343da06d0515e1f
Kacper <kacper.rajewski5@gmail.com>
2026-03-08 21:15:25 +0100

    download init

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -61,9 +61,20 @@ func main() {
 		Commands: []*cli.Command{
 			{
 				Name:  "list",
-				Usage: "List your currently watching anime with episode progress",
+				Usage: "List your currently watching Anime",
 				Action: func(ctx context.Context, cmd *cli.Command) error {
 					err := animeList(c, ctx)
+					if err != nil {
+						return err
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "download",
+				Usage: "Download all of your missing episodes",
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					err := download(c, ctx)
 					if err != nil {
 						return err
 					}
@@ -76,6 +87,74 @@ func main() {
 	if err := app.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func download(c *mal.Client, ctx context.Context) error {
+	anime, _, err := c.User.AnimeList(ctx, "@me",
+		mal.Fields{"list_status"},
+		mal.AnimeStatusWatching,
+		mal.SortAnimeListByListUpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range anime {
+		currentEpisode := episodeCount(item.Anime.Title)
+		episodesWatched := item.Status.NumEpisodesWatched
+
+		for i := episodesWatched + 1; i <= currentEpisode; i++ {
+			fmt.Println(i)
+		}
+	}
+
+	// cfg := torrent.NewDefaultClientConfig()
+	// cfg.DataDir = "/home/kacper/anime"
+	//
+	// client, err := torrent.NewClient(cfg)
+	// if err != nil {
+	// 	log.Fatalf("Error creating torrent client: %v", err)
+	// }
+	// defer client.Close()
+	//
+	// infoHashHex := "68ba5c2f758f0b5d1106e00e855b382dd5ce124c"
+	//
+	// trackers := "&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce"
+	//
+	// magnetURI := fmt.Sprintf("magnet:?xt=urn:btih:%s%s", infoHashHex, trackers)
+	//
+	// t, err := client.AddMagnet(magnetURI)
+	// if err != nil {
+	// 	log.Fatalf("Error adding magnet: %v", err)
+	// }
+	//
+	// fmt.Println("Fetching torrent metadata (waiting for DHT/Trackers)...")
+	//
+	// // Wait for the client to get the torrent info (metadata)
+	// <-t.GotInfo()
+	// fmt.Printf("Metadata acquired! Name: %s\n", t.Info().Name)
+	//
+	// t.DownloadAll()
+	//
+	// go func() {
+	// 	for {
+	// 		bytesCompleted := t.BytesCompleted()
+	// 		totalLength := t.Info().TotalLength()
+	// 		percentage := float64(bytesCompleted) / float64(totalLength) * 100
+	//
+	// 		fmt.Printf("\rDownloading: %.2f%% (%d / %d bytes)", percentage, bytesCompleted, totalLength)
+	//
+	// 		if bytesCompleted == totalLength {
+	// 			break
+	// 		}
+	// 		time.Sleep(2 * time.Second)
+	// 	}
+	// }()
+	//
+	// // 7. Block the main thread until the specific torrent finishes
+	// client.WaitAll()
+	// fmt.Println("\nDownload complete!")
+	return nil
 }
 
 func animeList(c *mal.Client, ctx context.Context) error {
