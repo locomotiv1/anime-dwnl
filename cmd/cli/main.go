e815b2b6bb63f41b86a39add7d42debfa2f9c852
Kacper <kacper.rajewski5@gmail.com>
2026-03-09 17:47:58 +0100

    working magnets
    magnets work with list of trused groups and 1080p releases, in the
    future user migth be able to customize them

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/nstratos/go-myanimelist/mal"
 	"github.com/urfave/cli/v3"
@@ -52,6 +54,17 @@ type AniListResponse struct {
 	} `json:"data"`
 }
 
+type Rss struct {
+	Items []RssItem `xml:"channel>item"`
+}
+
+type RssItem struct {
+	Title    string `xml:"title"`
+	Link     string `xml:"link"`
+	InfoHash string `xml:"infoHash"`
+	Seeders  int    `xml:"seeders"`
+}
+
 func main() {
 	oauth2Client := getAuthClient()
 	c := mal.NewClient(oauth2Client)
@@ -90,7 +103,54 @@ func main() {
 	}
 }
 
+// Notice we added trustedUploaders []string here!
+func getTorrent(requestURL string, trustedUploaders []string) string {
+	// 1. Send the HTTP Request
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return ""
+	}
+	req.Header.Set("User-Agent", "Anime-Download-CLI/1.0")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	// 2. Read the response & Parse XML
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	var feed Rss
+	if err := xml.Unmarshal(bodyBytes, &feed); err != nil {
+		return ""
+	}
+
+	// 3. Filter for 1080p AND Trusted Uploaders
+	for _, item := range feed.Items {
+		// First, verify it is 1080p
+		if strings.Contains(item.Title, "1080p") {
+			// Then, check if the title contains ANY of our trusted groups
+			for _, group := range trustedUploaders {
+				// Using strings.Contains allows it to match "[SubsPlease]" even
+				// if it's buried in a long title string.
+				if strings.Contains(item.Title, group) {
+					// We found a match! Return the magnet link immediately.
+					return fmt.Sprintf("magnet:?xt=urn:btih:%s", item.InfoHash)
+				}
+			}
+		}
+	}
+
+	return "" // Return blank if no matching torrents were found
+}
+
 func fetchTorrent(c *mal.Client, ctx context.Context) error {
+	trustedGroups := []string{"SubsPlease", "Erai-raws", "Judas"}
 	anime, _, err := c.User.AnimeList(ctx, "@me",
 		mal.Fields{"list_status"},
 		mal.AnimeStatusWatching,
@@ -107,12 +167,14 @@ func fetchTorrent(c *mal.Client, ctx context.Context) error {
 		if episodesWatched == 0 {
 			searchQuery := item.Anime.Title
 			requestURL := fmt.Sprintf("https://nyaa.si/?page=rss&q=%s&c=1_2&f=0&s=seeders&o=desc", url.QueryEscape(searchQuery))
-			println(requestURL)
+			magnet := getTorrent(requestURL, trustedGroups)
+			println(magnet)
 		} else {
 			for i := episodesWatched + 1; i <= currentEpisode; i++ {
 				searchQuery := fmt.Sprintf("%s 0%d", item.Anime.Title, i) // it fucks up when u wanna search episodes with more than 9 episodes
 				requestURL := fmt.Sprintf("https://nyaa.si/?page=rss&q=%s&c=1_2&f=0&s=seeders&o=desc", url.QueryEscape(searchQuery))
-				println(requestURL)
+				magnet := getTorrent(requestURL, trustedGroups)
+				println(magnet)
 			}
 		}
 	}
