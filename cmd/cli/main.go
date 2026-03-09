3cdf2ca99ddbee42dd77d53b9e452005fc6e2b67
Kacper <kacper.rajewski5@gmail.com>
2026-03-09 16:28:00 +0100

    working rss request

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/nstratos/go-myanimelist/mal"
@@ -74,7 +75,7 @@ func main() {
 				Name:  "download",
 				Usage: "Download all of your missing episodes",
 				Action: func(ctx context.Context, cmd *cli.Command) error {
-					err := download(c, ctx)
+					err := fetchTorrent(c, ctx)
 					if err != nil {
 						return err
 					}
@@ -89,7 +90,7 @@ func main() {
 	}
 }
 
-func download(c *mal.Client, ctx context.Context) error {
+func fetchTorrent(c *mal.Client, ctx context.Context) error {
 	anime, _, err := c.User.AnimeList(ctx, "@me",
 		mal.Fields{"list_status"},
 		mal.AnimeStatusWatching,
@@ -104,56 +105,11 @@ func download(c *mal.Client, ctx context.Context) error {
 		episodesWatched := item.Status.NumEpisodesWatched
 
 		for i := episodesWatched + 1; i <= currentEpisode; i++ {
-			fmt.Println(i)
+			searchQuery := fmt.Sprintf("%s 0%d", item.Anime.Title, i)
+			requestURL := fmt.Sprintf("https://nyaa.si/?page=rss&q=%s&c=1_2&f=0", url.QueryEscape(searchQuery))
+			println(requestURL)
 		}
 	}
-
-	// cfg := torrent.NewDefaultClientConfig()
-	// cfg.DataDir = "/home/kacper/anime"
-	//
-	// client, err := torrent.NewClient(cfg)
-	// if err != nil {
-	// 	log.Fatalf("Error creating torrent client: %v", err)
-	// }
-	// defer client.Close()
-	//
-	// infoHashHex := "68ba5c2f758f0b5d1106e00e855b382dd5ce124c"
-	//
-	// trackers := "&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce"
-	//
-	// magnetURI := fmt.Sprintf("magnet:?xt=urn:btih:%s%s", infoHashHex, trackers)
-	//
-	// t, err := client.AddMagnet(magnetURI)
-	// if err != nil {
-	// 	log.Fatalf("Error adding magnet: %v", err)
-	// }
-	//
-	// fmt.Println("Fetching torrent metadata (waiting for DHT/Trackers)...")
-	//
-	// // Wait for the client to get the torrent info (metadata)
-	// <-t.GotInfo()
-	// fmt.Printf("Metadata acquired! Name: %s\n", t.Info().Name)
-	//
-	// t.DownloadAll()
-	//
-	// go func() {
-	// 	for {
-	// 		bytesCompleted := t.BytesCompleted()
-	// 		totalLength := t.Info().TotalLength()
-	// 		percentage := float64(bytesCompleted) / float64(totalLength) * 100
-	//
-	// 		fmt.Printf("\rDownloading: %.2f%% (%d / %d bytes)", percentage, bytesCompleted, totalLength)
-	//
-	// 		if bytesCompleted == totalLength {
-	// 			break
-	// 		}
-	// 		time.Sleep(2 * time.Second)
-	// 	}
-	// }()
-	//
-	// // 7. Block the main thread until the specific torrent finishes
-	// client.WaitAll()
-	// fmt.Println("\nDownload complete!")
 	return nil
 }
 
