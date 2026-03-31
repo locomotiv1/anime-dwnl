13873b0fe55a1d7bfacb4639bb256f0fa272e25c
Kacper <kacper.rajewski5@gmail.com>
2026-03-31 19:09:04 +0200

    code refactor

--- cmd/anime/api.go
+++ cmd/anime/api.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
 const query = `
 query ($search: String) {
   Media (search: $search, type: ANIME) {
@@ -15,6 +27,9 @@ query ($search: String) {
   }
 }
 `
+
+// Reusable HTTP client
+var httpClient = &http.Client{}
 
 type GraphQLRequest struct {
 	Query     string                 `json:"query"`
@@ -46,4 +61,84 @@ type RssItem struct {
 	Link     string `xml:"link"`
 	InfoHash string `xml:"infoHash"`
 	Seeders  int    `xml:"seeders"`
+}
+
+func episodeCount(title string) int {
+	requestBody := GraphQLRequest{
+		Query: query,
+		Variables: map[string]interface{}{
+			"search": title,
+		},
+	}
+
+	jsonBytes, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Fatalf("Failed to marshal JSON: %v", err)
+	}
+
+	resp, err := httpClient.Post(
+		"https://graphql.anilist.co",
+		"application/json",
+		bytes.NewBuffer(jsonBytes),
+	)
+	if err != nil {
+		log.Fatalf("HTTP request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, _ := io.ReadAll(resp.Body)
+
+	var result AniListResponse
+	if err := json.Unmarshal(bodyBytes, &result); err != nil {
+		log.Fatalf("Failed to parse response: %v", err)
+	}
+
+	if result.Data.Media.Title.Romaji == "" && result.Data.Media.Title.English == "" {
+		return 0
+	}
+
+	media := result.Data.Media
+	if media.Status == "NOT_YET_RELEASED" {
+		return 0
+	} else if media.Status == "RELEASING" && media.NextAiringEpisode != nil {
+		return media.NextAiringEpisode.Episode - 1
+	}
+	return media.Episodes
+}
+
+func getTorrent(searchQuery string, trustedUploaders []string) string {
+	requestURL := fmt.Sprintf("https://nyaa.si/?page=rss&q=%s&c=1_2&f=0&s=seeders&o=desc", url.QueryEscape(searchQuery))
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return ""
+	}
+	req.Header.Set("User-Agent", "Anime-Download-CLI/1.0")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	var feed Rss
+	if err := xml.Unmarshal(bodyBytes, &feed); err != nil {
+		return ""
+	}
+
+	for _, item := range feed.Items {
+		if strings.Contains(item.Title, "1080p") {
+			for _, group := range trustedUploaders {
+				if strings.Contains(item.Title, group) {
+					trackers := "&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"
+					return fmt.Sprintf("magnet:?xt=urn:btih:%s%s", item.InfoHash, trackers)
+				}
+			}
+		}
+	}
+	return ""
 }
--- cmd/anime/main.go
+++ cmd/anime/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"encoding/xml"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 	"sort"
@@ -28,7 +22,11 @@ type DownloadTask struct {
 	Magnet string
 }
 
-// Removes illegal characters from Anime titles so folder creation doesn't crash on Windows
+type FileMoveTask struct {
+	OldPath string
+	NewPath string
+}
+
 func sanitizeFolder(name string) string {
 	invalidChars := []string{"<", ">", ":", "\"", "/", "\\", "|", "?", "*"}
 	for _, char := range invalidChars {
@@ -49,11 +47,7 @@ func main() {
 				Name:  "list",
 				Usage: "List your currently watching Anime",
 				Action: func(ctx context.Context, cmd *cli.Command) error {
-					err := animeList(c, ctx)
-					if err != nil {
-						return err
-					}
-					return nil
+					return animeList(c, ctx)
 				},
 			},
 			{
@@ -74,10 +68,12 @@ func main() {
 				Action: func(ctx context.Context, cmd *cli.Command) error {
 					listNum := int(cmd.Int("listing"))
 					count := int(cmd.Int("count"))
+
 					if listNum == 0 {
 						fmt.Println("You are downloading your entire watch list, this might take a while")
 						fmt.Println("Use -l <number> to download specific anime")
 					}
+
 					var wg sync.WaitGroup
 					s := chin.New().WithWait(&wg)
 					go s.Start()
@@ -85,6 +81,7 @@ func main() {
 					tasks, err := fetchTorrent(c, ctx, listNum, count)
 					s.Stop()
 					wg.Wait()
+
 					if err != nil {
 						return err
 					}
@@ -92,6 +89,7 @@ func main() {
 						fmt.Println("No new episodes to download.")
 						return nil
 					}
+
 					download(tasks)
 					return nil
 				},
@@ -104,83 +102,59 @@ func main() {
 	}
 }
 
-func getTorrent(requestURL string, trustedUploaders []string) string {
-	req, err := http.NewRequest("GET", requestURL, nil)
-	if err != nil {
-		return ""
-	}
-	req.Header.Set("User-Agent", "Anime-Download-CLI/1.0")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return ""
-	}
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return ""
-	}
-
-	var feed Rss
-	if err := xml.Unmarshal(bodyBytes, &feed); err != nil {
-		return ""
-	}
-
-	for _, item := range feed.Items {
-		if strings.Contains(item.Title, "1080p") {
-			for _, group := range trustedUploaders {
-				if strings.Contains(item.Title, group) {
-					trackers := "&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"
-					return fmt.Sprintf("magnet:?xt=urn:btih:%s%s", item.InfoHash, trackers)
-				}
-			}
-		}
-	}
-
-	return ""
-}
-
 func download(tasks []DownloadTask) {
-	clientConfig := torrent.NewDefaultClientConfig()
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatalf("could now find home directory %v", err)
+		log.Fatalf("could not find home directory: %v", err)
 	}
 	downloadFolder := filepath.Join(homeDir, "anime")
+
+	clientConfig := torrent.NewDefaultClientConfig()
 	clientConfig.DataDir = downloadFolder
 
 	c, err := torrent.NewClient(clientConfig)
 	if err != nil {
 		log.Fatalf("Error creating torrent client: %v", err)
 	}
-	defer c.Close()
 
 	fmt.Printf("Queuing up %d episodes for download...\n", len(tasks))
 
-	// create a map to link a torrent's unique hash back to its Anime Title
 	titleMap := make(map[string]string)
-
 	for _, task := range tasks {
 		t, err := c.AddMagnet(task.Magnet)
 		if err != nil {
 			log.Printf("Error adding magnet: %v\n", err)
 			continue
 		}
-
-		// Store the clean anime title in our map using the torrent's hash as the key
 		titleMap[t.InfoHash().String()] = sanitizeFolder(task.Title)
-
 		go func(currTorrent *torrent.Torrent) {
 			<-currTorrent.GotInfo()
 			currTorrent.DownloadAll()
 		}(t)
 	}
 
-	go func() {
-		for {
-			time.Sleep(3 * time.Second)
+	// Use a done channel to cleanly exit the monitor loop
+	done := make(chan struct{})
+	go monitorProgress(c, done)
+
+	c.WaitAll()
+	close(done)
+	fmt.Println("All torrents successfully downloaded!")
+
+	// Figure out paths before closing the client
+	moves := getMoveTasks(c, downloadFolder, titleMap)
+
+	c.Close()
+
+	organizeFiles(moves)
+}
+
+func monitorProgress(c *torrent.Client, done chan struct{}) {
+	for {
+		select {
+		case <-done:
+			return
+		case <-time.After(3 * time.Second):
 			fmt.Print("\033[H\033[2J")
 			fmt.Println("\n--- Current Downloads ---")
 			activeTorrents := c.Torrents()
@@ -200,7 +174,6 @@ func download(tasks []DownloadTask) {
 
 				if total > 0 {
 					percent := float64(completed) / float64(total) * 100
-
 					if percent == 100 {
 						fmt.Printf("[ DONE ] %s\n", t.Info().Name)
 					} else {
@@ -209,25 +182,19 @@ func download(tasks []DownloadTask) {
 				}
 			}
 		}
-	}()
-
-	c.WaitAll()
-	fmt.Println("All torrents successfully downloaded!")
-
-	// Gather all the file paths BEFORE we close the client
-	type MoveTask struct {
-		OldPath string
-		NewPath string
 	}
-	var moves []MoveTask
+}
+
+func getMoveTasks(c *torrent.Client, downloadFolder string, titleMap map[string]string) []FileMoveTask {
+	var moves []FileMoveTask
 
 	for _, t := range c.Torrents() {
 		if t.Info() == nil {
 			continue
 		}
 
-		animeTitle, exists := titleMap[t.InfoHash().String()]
-		if !exists || animeTitle == "" {
+		animeTitle := titleMap[t.InfoHash().String()]
+		if animeTitle == "" {
 			continue
 		}
 
@@ -238,16 +205,16 @@ func download(tasks []DownloadTask) {
 		newPath := filepath.Join(animeFolder, t.Info().Name)
 
 		if oldPath != newPath {
-			moves = append(moves, MoveTask{OldPath: oldPath, NewPath: newPath})
+			moves = append(moves, FileMoveTask{OldPath: oldPath, NewPath: newPath})
 		}
 	}
+	return moves
+}
 
-	c.Close()
-
+func organizeFiles(moves []FileMoveTask) {
 	fmt.Println("Organizing files into folders...")
 	for _, m := range moves {
-		err := os.Rename(m.OldPath, m.NewPath)
-		if err != nil {
+		if err := os.Rename(m.OldPath, m.NewPath); err != nil {
 			log.Printf("Failed to move %s: %v\n", m.OldPath, err)
 		}
 	}
@@ -267,13 +234,14 @@ func fetchTorrent(c *mal.Client, ctx context.Context, targetIndex int, count int
 		return nil, err
 	}
 	if targetIndex > len(anime) {
-		return nil, fmt.Errorf("Invalid list number")
+		return nil, fmt.Errorf("invalid list number")
 	}
 
 	for i, item := range anime {
 		if targetIndex > 0 && i != (targetIndex-1) {
 			continue
 		}
+
 		currentEpisode := episodeCount(item.Anime.Title)
 		episodesWatched := item.Status.NumEpisodesWatched
 
@@ -281,10 +249,8 @@ func fetchTorrent(c *mal.Client, ctx context.Context, targetIndex int, count int
 			if count > 0 {
 				fmt.Printf("\n[Notice] '%s' has finished airing. Ignoring the episode count flag and downloading the full batch instead.\n", item.Anime.Title)
 			}
-			searchQuery := item.Anime.Title
-			requestURL := fmt.Sprintf("https://nyaa.si/?page=rss&q=%s&c=1_2&f=0&s=seeders&o=desc", url.QueryEscape(searchQuery))
-			magnet := getTorrent(requestURL, trustedGroups)
-			if magnet != "" {
+
+			if magnet := getTorrent(item.Anime.Title, trustedGroups); magnet != "" {
 				tasks = append(tasks, DownloadTask{Title: item.Anime.Title, Magnet: magnet})
 			}
 
@@ -293,11 +259,11 @@ func fetchTorrent(c *mal.Client, ctx context.Context, targetIndex int, count int
 			if count > 0 && (episodesWatched+count) < currentEpisode {
 				endEpisode = episodesWatched + count
 			}
+
 			for i := episodesWatched + 1; i <= endEpisode; i++ {
 				searchQuery := fmt.Sprintf("%s %02d", item.Anime.Title, i)
-				requestURL := fmt.Sprintf("https://nyaa.si/?page=rss&q=%s&c=1_2&f=0&s=seeders&o=desc", url.QueryEscape(searchQuery))
-				magnet := getTorrent(requestURL, trustedGroups)
-				if magnet != "" {
+
+				if magnet := getTorrent(searchQuery, trustedGroups); magnet != "" {
 					tasks = append(tasks, DownloadTask{Title: item.Anime.Title, Magnet: magnet})
 				}
 			}
@@ -305,7 +271,7 @@ func fetchTorrent(c *mal.Client, ctx context.Context, targetIndex int, count int
 			continue
 		}
 	}
-	return tasks, err
+	return tasks, nil
 }
 
 func animeList(c *mal.Client, ctx context.Context) error {
@@ -320,69 +286,21 @@ func animeList(c *mal.Client, ctx context.Context) error {
 
 	if len(anime) == 0 {
 		fmt.Println("You aren't watching anything right now!")
-	} else {
-		fmt.Println("\n Currently Watching:")
-		fmt.Println("--------------------------------------------------")
-
-		for i, item := range anime {
-			currentEpisodes := episodeCount(item.Anime.Title)
-			fmt.Printf("%d - %s (Ep: %d) / %d \n",
-				i+1,
-				item.Anime.Title,
-				item.Status.NumEpisodesWatched,
-				currentEpisodes,
-			)
-		}
-		fmt.Println("--------------------------------------------------")
+		return nil
 	}
 
+	fmt.Println("\n Currently Watching:")
+	fmt.Println("--------------------------------------------------")
+
+	for i, item := range anime {
+		currentEpisodes := episodeCount(item.Anime.Title)
+		fmt.Printf("%d - %s (Ep: %d) / %d \n",
+			i+1,
+			item.Anime.Title,
+			item.Status.NumEpisodesWatched,
+			currentEpisodes,
+		)
+	}
+	fmt.Println("--------------------------------------------------")
 	return nil
-}
-
-func episodeCount(title string) int {
-	requestBody := GraphQLRequest{
-		Query: query,
-		Variables: map[string]interface{}{
-			"search": title,
-		},
-	}
-
-	jsonBytes, err := json.Marshal(requestBody)
-	if err != nil {
-		log.Fatalf("Failed to marshal JSON: %v", err)
-	}
-
-	resp, err := http.Post(
-		"https://graphql.anilist.co",
-		"application/json",
-		bytes.NewBuffer(jsonBytes),
-	)
-	if err != nil {
-		log.Fatalf("HTTP request failed: %v", err)
-	}
-	defer resp.Body.Close()
-
-	bodyBytes, _ := io.ReadAll(resp.Body)
-
-	var result AniListResponse
-	if err := json.Unmarshal(bodyBytes, &result); err != nil {
-		log.Fatalf("Failed to parse response: %v", err)
-	}
-	if result.Data.Media.Title.Romaji == "" && result.Data.Media.Title.English == "" {
-		return 0
-	}
-
-	media := result.Data.Media
-
-	var releasedEpisodes int
-
-	if media.Status == "NOT_YET_RELEASED" {
-		releasedEpisodes = 0
-	} else if media.Status == "RELEASING" && media.NextAiringEpisode != nil {
-		releasedEpisodes = media.NextAiringEpisode.Episode - 1
-	} else {
-		releasedEpisodes = media.Episodes
-	}
-
-	return releasedEpisodes
 }
